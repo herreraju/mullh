2feefa89f230885cb17d1236bdec22d869b83f3c
Justin Herrera <Herreraju@mullenmail.com>
2016-02-04 07:27:36 -0700

    gsd

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.it-o').slick({
+    arrows: true,
+    dots: true,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='sitePics/left.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='sitePics/right.png'>"
+    });
+});
